fcd4f709951e1e951602beffa8e52c06447c17b9
Evgeniy <timeinvest27@gmail.com>
2019-09-22 11:49:22 +0300

    Метод отправки сообщения о новом пользователе

--- ChatAspnetCoreAuthentication/Hubs/ChatHub.cs
+++ ChatAspnetCoreAuthentication/Hubs/ChatHub.cs
@@ -7,6 +7,12 @@ namespace SignalRChat.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
+
+        public async Task NewUser(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
