2dbaa01fb7001c9552f2fa577886a456cb8a04e3
Evgeniy <timeinvest27@gmail.com>
2019-09-22 11:48:52 +0300

    Модель сообщений

---
+++ ChatAspnetCoreAuthentication/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAspnetCoreAuthentication.Models
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        //public User User { get; set; }
+        public string NameUser { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
