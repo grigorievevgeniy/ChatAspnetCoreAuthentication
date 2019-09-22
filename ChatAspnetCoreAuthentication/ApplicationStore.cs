2afa2874052cf1015082caf269271b8ab14a7fe3
Evgeniy <timeinvest27@gmail.com>
2019-09-22 16:33:06 +0300

    Add ApplicationStore

---
+++ ChatAspnetCoreAuthentication/ApplicationStore.cs
@@ -0,0 +1,18 @@
+﻿using ChatAspnetCoreAuthentication.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAspnetCoreAuthentication
+{
+    public class ApplicationStore
+    {
+        ApplicationDbContext _applicationDbContext;
+
+        public ApplicationStore(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+    }
+}
