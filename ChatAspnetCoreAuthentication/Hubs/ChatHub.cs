c71f3d06d699a5b5ef7ea4a31ec2044fc38f6427
Evgeniy <timeinvest27@gmail.com>
2019-09-26 15:05:58 +0300

    Роль block. Запрет на отпраку сообщений

--- ChatAspnetCoreAuthentication/Hubs/ChatHub.cs
+++ ChatAspnetCoreAuthentication/Hubs/ChatHub.cs
@@ -1,35 +1,51 @@
 ﻿using ChatAspnetCoreAuthentication;
+using ChatAspnetCoreAuthentication.Controllers;
 using ChatAspnetCoreAuthentication.Data;
 using ChatAspnetCoreAuthentication.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
     [Authorize]
-    [Authorize(Roles = "admin")]
+    //[Authorize(Roles = "admin")]
     public class ChatHub : Hub
     {
         private ApplicationStore _store;
+        WorkWithRoles workWithRoles;
+        UserManager<IdentityUser> _userManager;
 
-        public ChatHub(ApplicationStore store)
+        public ChatHub(ApplicationStore store, UserManager<IdentityUser> userManager)
         {
             _store = store;
+            _userManager = userManager;
         }
 
         public async Task SendMessage(string user, string message)
         {
-            if (!message.StartsWith("//")) 
-            {
-                await Clients.All.SendAsync("ReceiveMessage", user, message);
+            IdentityUser identityUser = await _userManager.FindByNameAsync(user);
 
-                AddMessage(new ChatMessage() { SenderId = user, Text = message });
+            if (!await _userManager.IsInRoleAsync(identityUser, "block"))
+            {
+                if (!message.StartsWith("//"))
+                {
+                    await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+                    AddMessage(new ChatMessage() { SenderId = user, Text = message });
+                }
+                else
+                {
+                    await Clients.Caller.SendAsync("ReceiveMessage", user, "Команды пока не реализованны");
+                }
+
             }
             else
             {
-                await Clients.Caller.SendAsync("ReceiveMessage", user, "Команды пока не реализованны");
+                await Clients.Caller.SendAsync("ReceiveMessage", user, "Вы заблокированны и не можете отправлять сообщения. Обратитесь к модератору или администратору.");
             }
+
 
         }
 
