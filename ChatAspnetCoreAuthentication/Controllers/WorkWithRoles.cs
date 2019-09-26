195e11961ac37e2259531fab42bc4d199541585d
Evgeniy <timeinvest27@gmail.com>
2019-09-26 15:07:05 +0300

    Отдельный класс для работы с ролями. Пока не задействован.

---
+++ ChatAspnetCoreAuthentication/Controllers/WorkWithRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAspnetCoreAuthentication.Controllers
+{
+    public class WorkWithRoles
+    {
+        public UserManager<IdentityUser> _userManager;
+        public RoleManager<IdentityRole> _roleManager;
+
+        public WorkWithRoles()
+        {
+        }
+
+        public WorkWithRoles(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async void AddRoleUserForRigister(IdentityUser user)
+        {
+            await _userManager.AddToRoleAsync(user, "user");
+        }
+
+
+
+    }
+}
