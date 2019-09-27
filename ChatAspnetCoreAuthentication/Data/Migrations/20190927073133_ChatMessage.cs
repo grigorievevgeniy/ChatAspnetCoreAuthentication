cfc1f356da8d0d1467e8a7b98f09548956f5d9fc
Evgeniy <timeinvest27@gmail.com>
2019-09-27 11:58:08 +0300

    Миграция

---
+++ ChatAspnetCoreAuthentication/Data/Migrations/20190927073133_ChatMessage.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatAspnetCoreAuthentication.Data.Migrations
+{
+    public partial class ChatMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChatName",
+                table: "ChatRooms",
+                newName: "RoomName");
+
+            migrationBuilder.RenameColumn(
+                name: "ChatId",
+                table: "ChatRooms",
+                newName: "RoomId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "ChatRooms",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "ChatMessages",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomId",
+                table: "ChatMessages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "ChatMessages",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "ChatRooms");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "ChatMessages");
+
+            migrationBuilder.RenameColumn(
+                name: "RoomName",
+                table: "ChatRooms",
+                newName: "ChatName");
+
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "ChatRooms",
+                newName: "ChatId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- ChatAspnetCoreAuthentication/Data/Migrations/20190927080812_MigrateDB.Designer.cs
+++ ChatAspnetCoreAuthentication/Data/Migrations/20190927080812_MigrateDB.Designer.cs
@@ -4,14 +4,16 @@ using ChatAspnetCoreAuthentication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ChatAspnetCoreAuthentication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190927080812_MigrateDB")]
+    partial class MigrateDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
