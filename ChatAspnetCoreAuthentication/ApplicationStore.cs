4789e9ce966a380899eb772200e0f363b871b55e
Evgeniy <timeinvest27@gmail.com>
2019-10-01 08:58:41 +0300

    Проверка является ли юзер участником комнаты

--- ChatAspnetCoreAuthentication/ApplicationStore.cs
+++ ChatAspnetCoreAuthentication/ApplicationStore.cs
@@ -44,5 +44,17 @@ namespace ChatAspnetCoreAuthentication
             ChatRoom room = _applicationDbContext.ChatRooms.Where(x => x.RoomName == nameRoom).FirstOrDefault();
             return room.OwnerId;
         }
+
+        internal bool CheckUserMemberRoom(string nameRoom, string UserId)
+        {
+            string RoomId = _applicationDbContext.ChatRooms.Where(x => x.RoomName == nameRoom).FirstOrDefault().ToString();
+
+            ChatUser chatUser = _applicationDbContext.ChatUsers.Where(x => x.UserId == UserId && x.ChatId == RoomId).FirstOrDefault();
+
+            if (chatUser == null)
+                return false;
+
+            return true;
+        }
     }
 }
