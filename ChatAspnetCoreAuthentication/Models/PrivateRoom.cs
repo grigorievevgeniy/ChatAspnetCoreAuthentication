468be34dbdda54ef91248a725ad1140b46ebf3ff
Evgeniy <timeinvest27@gmail.com>
2019-10-01 15:41:35 +0300

    Таблица для приватных комнат

---
+++ ChatAspnetCoreAuthentication/Models/PrivateRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAspnetCoreAuthentication.Models
+{
+    public class PrivateRoom
+    {
+        [Key]
+        public string User1Id { get; set; }
+        [Key]
+        public string User2Id { get; set; }
+    }
+}
