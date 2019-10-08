d6e24fd23bb700a3b273a853de95fdbb62010642
Evgeniy <timeinvest27@gmail.com>
2019-10-08 11:39:17 +0300

    Новый класс для команд

---
+++ ChatAspnetCoreAuthentication/Models/ChatCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAspnetCoreAuthentication.Models
+{
+    public class ChatCommand
+    {
+        delegate void RunCommand();
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public RunCommand RunCommand { get; set; }
+
+    }
+}
